f5f7211e45463e47a41864f114d925a637c792b0
Shaunbus <sjbucyk@live.ca>
2021-02-18 12:34:50 -0800

    Extracting current username, copying is in working form

--- ValheimBackup/Program.cs
+++ ValheimBackup/Program.cs
@@ -6,10 +6,14 @@ namespace ValheimBackup
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string sourceDir = @"C:\Users\Shaun\Desktop\Projects\ValheimBackup\ValheimBackup\Test\characters.rar";
-            string backupDir = @"C:\Users\Shaun\Desktop\Projects\ValheimBackup\ValheimBackup\Test\characters" + 1 + @".rar";
+            // Extracting current UserName for accurate pathname for source and target directory
+            // Setting up paths with Username in strings after
 
+            string currentUser = Environment.UserName;
+            string sourceDir = @"C:\Users\" + currentUser + @"\Desktop\Projects\ValheimBackup\ValheimBackup\Test\characters.rar";
+            string backupDir = @"C:\Users\" + currentUser + @"\Desktop\Projects\ValheimBackup\ValheimBackup\Test\characters" + 1 + @".rar";
+
+            //Copying File
             System.IO.File.Copy(sourceDir, backupDir);
            
         }

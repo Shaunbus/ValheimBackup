f4e00f178806c37077bc284068875a170fe4326f
Shaunbus <sjbucyk@live.ca>
2021-02-18 12:18:16 -0800

    Rudimentary file copy added to test permissions

--- ValheimBackup/Program.cs
+++ ValheimBackup/Program.cs
@@ -7,6 +7,11 @@ namespace ValheimBackup
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string sourceDir = @"C:\Users\Shaun\Desktop\Projects\ValheimBackup\ValheimBackup\Test\characters.rar";
+            string backupDir = @"C:\Users\Shaun\Desktop\Projects\ValheimBackup\ValheimBackup\Test\characters" + 1 + @".rar";
+
+            System.IO.File.Copy(sourceDir, backupDir);
+           
         }
     }
 }
